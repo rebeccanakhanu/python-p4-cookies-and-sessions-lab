088e039d9ceef60533362edaad5885238b812c23
rebeccanakhanu <rebeccanakhanu@gmail.com>
2023-07-09 23:53:30 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,34 +1,32 @@
-#!/usr/bin/env python3
-
-from flask import Flask, make_response, jsonify, session
+from flask import Flask, request, session, jsonify, make_response
 from flask_migrate import Migrate
-
 from models import db, Article, User
 
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.json.compact = False
+app.secret_key = b'?w\x85Z\x08Q\xbdO\xb8\xa9\xb65Kj\xa9_'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
-migrate = Migrate(app, db)
-
+migrate = Migrate(app,db)
 db.init_app(app)
 
-@app.route('/clear')
+@app.route('/articles/<int:id>', methods=['GET'])
+def get_article(id):
+    session['page_views'] = session.get('page_views', 0) + 1
+    
+    if session['page_views'] <= 3:
+        article= Article.query.filter_by(id=id).first()
+        article_dict = article.to_dict()
+        return make_response(jsonify(article_dict),200)
+    else:
+        return (jsonify({'message':'Maximum pageview limit reached'}),401)
+    
+@app.route('/clear', methods=['POST'])
 def clear_session():
-    session['page_views'] = 0
-    return {'message': '200: Successfully cleared session data.'}, 200
-
-@app.route('/articles')
-def index_articles():
-
-    pass
-
-@app.route('/articles/<int:id>')
-def show_article(id):
-
-    pass
-
+    session.clear()
+    return jsonify({'message': 'Session cleared'})
+        
 if __name__ == '__main__':
     app.run(port=5555)
+    
